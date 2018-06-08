6c0abc5f1983f70ac7e5294ece24d85f837b5fc6
Hui Yi Chen <hychen12@gmail.com>
2018-06-08 15:14:06 +0800

    6/7~8

--- src/main/java/com/example/demo2/Login.java
+++ src/main/java/com/example/demo2/Login.java
@@ -1,13 +1,26 @@
 package com.example.demo2;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.HttpSessionRequiredException;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpSession;
+import java.util.UUID;
 
 @Controller
 public class Login {
     @RequestMapping(value="/")
     public String home(){
+        return "home";
+    }
+
+    @RequestMapping(value = "/uid")
+    public String sessionTest(HttpSession session){
+        UUID uid = (UUID) session.getAttribute("uid");
+        if(uid == null)
+            uid = UUID.randomUUID();
+        session.setAttribute("uid", uid);
         return "home";
     }
 }
--- src/test/java/com/example/demo2/Demo2ApplicationTests.java
+++ src/test/java/com/example/demo2/Demo2ApplicationTests.java
@@ -1,16 +1,55 @@
 package com.example.demo2;
 
+import org.apache.catalina.User;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.Serializable;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class Demo2ApplicationTests {
 
+    @Autowired
+    StringRedisTemplate stringRedisTemplate;
+    @Autowired
+    RedisTemplate redisTemplate;
+
     @Test
     public void contextLoads() {
+        System.out.println("test pass");
+    }
+
+    @Test
+    public void testReids() {
+        stringRedisTemplate.opsForValue().set("aaa","111");
+        Assert.assertEquals("111", stringRedisTemplate.opsForValue().get("aaa"));
+
+    }
+
+    @Test
+    public void testReidsObj() {
+        User u = new User("abc");
+        ValueOperations<String, User> operations = redisTemplate.opsForValue();
+        operations.set("usss",u);
+
+        Assert.assertEquals("usss", operations.get("usss").getName());
+
+    }
+
+    public class User implements Serializable {
+        private String name;
+        public User(String name){
+            this.name = name;
+        }
+        public String getName() {return name;}
     }
 
 }
